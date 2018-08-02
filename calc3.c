43d52116cc83ae66a61cd01491d4f0f136eca438
chrisp <chris@chrisprocter.co.uk>
2018-08-03 00:26:13 +0100

    add built in function support

--- calc3.c
+++ calc3.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -28,6 +30,9 @@ double eval_divide(Cell *c);
 double eval_power(Cell *c);
 double evaluate(Cell* c);
 
+double eval_avg(Cell *c);
+double eval_max(Cell *c);
+
 Cell * expression(int number ,char ** line);
 
 
@@ -47,6 +52,10 @@ struct ops opcodes[]={
     {"",&eval_value,&number}
 };
 
+struct ops symboltable[]={
+    {"avg",&eval_avg,&expression},
+    {"max",&eval_max,&expression},
+};
 
 char ** ltrim(char ** line){
     while((**line) == ' ' && (**line) != 0){
@@ -89,13 +98,10 @@ Cell * expression(int number ,char ** line){
     Cell * c;
     Cell * e;
 
-    //printf("subtract=%s\n",*line);
     e = opcodes[number+1].parse(number+1,line);
     while(is_symbol(opcodes[number].symbol, line)==1){
         (*line) += strlen(opcodes[number].symbol);
-        //printf("line=%s ===%s\n",*line, opcodes[number].symbol);
         c = new_cell(2);
-        //c->operator="-";
         c->operator=opcodes[number].eval;
         c->operand[0] = e; 
         c->operand[1] = opcodes[number+1].parse(number+1,line);
@@ -104,25 +110,30 @@ Cell * expression(int number ,char ** line){
     return e;
 }
 
-/*
-Cell * array(int number ,char ** line){
+
+Cell * array(char ** line){
     Cell * c;
-    Cell * e;
 
     //printf("subtract=%s\n",*line);
-    e = opcodes[number+1].parse(number+1,line);
-    while(strncmp(",",1))==0){
-        (*line) += 1 
-        //printf("line=%s ===%s\n",*line, opcodes[number].symbol);
-        c = new_cell(2);
+    //e = opcodes[number+1].parse(number+1,line);
+    c = new_cell(1);
+    c->operand[0] = opcodes[0].parse(0,line);
         //c->operator="-";
-        c->operator = array()
-        c->operand[0] = e; 
-        c->operand[1] = opcodes[number+1].parse(number+1,line);
-        e=c;
+    //c->operator=opcodes[number].eval;
+//    c->operand[0] = e; 
+    printf("array=%s\n",*line);
+    while(strncmp(*line,",",1)==0){
+        printf("array=%s\n",*line);
+        (*line) += 1;
+        //c->operand = reallocarray(c->value, c->length+1, sizeof(Cell*));
+        c->length++;
+        c->operand = (Cell **) realloc(c->operand, (c->length)*sizeof(Cell*));
+        c->operand[(c->length)-1] = opcodes[0].parse(0,line);
     }
-    return e;
-}*/
+    return c;
+}
+
+
 /*
 Cell * subtract(char ** line){
     Cell * c;
@@ -165,6 +176,7 @@ Cell * plus(char ** line){
 Cell * number(int num, char ** line){
     char * endptr;
     Cell * c;
+    size_t len;
 
     //printf("number=%s\n",*line);
     line = ltrim(line);
@@ -176,7 +188,9 @@ Cell * number(int num, char ** line){
         c = new_cell(0);
         //c->operator = 0;
         c->operator = &eval_value;
+        printf("value=%e\n", value);
         c->value = value;
+        c->length = 1;
     }else if(**line=='('){
         (*line)++;
         //printf("line1=%s\n",*line);
@@ -186,11 +200,58 @@ Cell * number(int num, char ** line){
         }
         (*line)++;
         //printf("line2=%s\n",*line);
+    }else if((len=strspn(*line,"abcdefghijklmnopqrstuvwxyz"))!=0){
+        printf("len=%ld line=%s\n",len,*line);
+        if(*((*line)+len)=='('){
+            //run function
+            int i=0;
+            while((symboltable[i].symbol!=NULL)&&(strncmp(symboltable[i].symbol,*line,len)!=0)){
+                i++;
+            }
+            (*line)+=len+1;
+            c = array(line);
+            c->operator = symboltable[i].eval;
+            if(**line!=')'){
+    		    fprintf(stderr,"unmatched parenthesis\n");
+            }
+            (*line)++;
+        }else if(*((*line)+len)=='='){
+            printf("= line=%s\n",*line);
+            //assign to symbol
+        }else{
+		fprintf(stderr,"failed to comprehend at %s\n",*line);
+
+        }
+        printf("line2=%s\n",*line);
     }else{
 		fprintf(stderr,"failed to read value at %s\n",*line);
 		exit(1);
     }
     return c;
+}
+
+double eval_avg(Cell *c){
+    double total=0;
+    printf("c->length=%d\n",c->length);
+    for(int i=0; i < c->length; i++){
+        total += evaluate(c->operand[i]);
+        printf("total=%e\n",total);
+    }
+    printf("avg=%e\n",total/c->length);
+    return total/c->length;
+}
+
+double eval_max(Cell *c){
+    double max=0;
+    double cur=0;
+    for(int i=0;i<c->length;i++){
+        cur+=evaluate(c->operand[i]);
+        if((i==0) || (cur>max)){
+            max=cur;
+        }
+    }
+
+    return max;
 }
 
 double eval_plus(Cell *c){
@@ -225,11 +286,11 @@ double evaluate(Cell* c){
 
 int main(int argc, char* argv[]){
 
+/*
     if(argc==1){
       printf("usage: %s [expression]\n",argv[0]);
       exit(1);
     }
-
     int arglen=0;
     for(int i=1;i<argc;i++){
         arglen+=strlen(argv[i]);
@@ -238,9 +299,9 @@ int main(int argc, char* argv[]){
     for(int i=1;i<argc;i++){
         strcat(line,argv[i]); 
     }
-
-//    char *line=" 1+ 1- -3";
-
+*/
+    char *line=" 1+ 1- -3+avg(1,2,3)";
+    printf("%s\n",line);
 //    Cell* c = subtract(&line);
     Cell* c = opcodes[0].parse(0,&line);
 
